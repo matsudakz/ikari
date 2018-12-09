14ee7bdb8a9ca069eb9fe2c2a56af22dbeae05e7
MATSUDA Kazuo <mzd.no.o@gmail.com>
2018-12-09 16:35:37 +0900

    RGB

--- webapi3/upload/tasks.py
+++ webapi3/upload/tasks.py
@@ -4,17 +4,23 @@ from poster.streaminghttp import register_openers
 import urllib2
 import json
 import time
+import subprocess
 
+MAX_RANGE=255
+SLEEP_TIME=1
+EMPATH_URL="https://api.webempath.net/v2/analyzeWav"
 
 # 解析メソッド
-def voice(file_path, apikey):
-    time.sleep(5)
-    url = "https://api.webempath.net/v2/analyzeWav"
+def max_func(n):
+    return n[1]
+
+def voice_rgb(file_path, apikey):
+    time.sleep(SLEEP_TIME)
     register_openers()
     items = [MultipartParam('apikey', apikey),
              MultipartParam.from_file('wav', file_path)]
     datagen, headers = multipart_encode(items)
-    request = urllib2.Request(url, datagen, headers)
+    request = urllib2.Request(EMPATH_URL, datagen, headers)
     readObject = urllib2.urlopen(request)  # type: object
 
     if readObject.getcode() != 200:
@@ -22,5 +28,53 @@ def voice(file_path, apikey):
 
     response = json.load(readObject)
     print(response)
+
+    if response['error'] != 0:
+        print("analyze error %d" % (response['error']))
+        return
+
+    list=[('anger', response['anger']),('joy', response['joy']),('sorrow', response['sorrow']),('calm', response['calm'])]
+    max_num=max(list, key=max_func)
+    if max_num[0] == 'calm':
+        rgb=int('ffffff', 16)
+    else:
+        red=(response['anger']+response['energy']/2)*(MAX_RANGE//50)
+        green=(response['joy']+response['energy']/2)*(MAX_RANGE//50)
+        blue=(response['sorrow']+response['energy']/2)*(MAX_RANGE//50)
+        rgb=(red<<16)+(green<<8)+blue
+
+    print("rgb=", rgb)
+
+    while True:
+        if subprocess.check_call(["gatttool","-b","24:0A:C4:07:84:3E","--char-write-req","-a","0x002a","-n", format(rgb, 'x')]) == 0:
+            break
+    if response["error"] > 0:
+        print(response["error"])
+
+def voice(file_path, apikey):
+    time.sleep(SLEEP_TIME)
+    register_openers()
+    items = [MultipartParam('apikey', apikey),
+             MultipartParam.from_file('wav', file_path)]
+    datagen, headers = multipart_encode(items)
+    request = urllib2.Request(EMPATH_URL, datagen, headers)
+    readObject = urllib2.urlopen(request)  # type: object
+
+    if readObject.getcode() != 200:
+        print("HTTP status %d" % (readObject.getcode()))
+
+    response = json.load(readObject)
+    print(response)
+
+    if response['error'] != 0:
+        print("analyze error %d" % (response['error']))
+        return
+
+    anger_num=response['anger']*(MAX_RANGE//50)
+    print(anger_num)
+    print(format(anger_num, 'x'))
+    while True:
+        if subprocess.check_call(["gatttool","-b","24:0A:C4:07:84:3E","--char-write-req","-a","0x002a","-n", format(anger_num, 'x')]) == 0:
+            break
     if response["error"] > 0:
         print(response["error"])
